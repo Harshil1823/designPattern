8040975d294fa9fc95c0faa5f3eb62dbf6c690d8
Dominick Ruggiero <domrugg124@gmail.com>
2023-02-24 15:36:05 -0500

    Java docing

--- command/Command.java
+++ command/Command.java
@@ -1,5 +1,8 @@
 package command;
-
+/**
+ * Represents a Command.
+ * @author javadoc
+ */
 public interface Command {
     public void execute();
 }
--- command/FiveMinCommand.java
+++ command/FiveMinCommand.java
@@ -1,12 +1,23 @@
 package command;
 
+/**
+ * Represents the five minute command.
+ * @author javadoc
+ */
 public class FiveMinCommand implements Command {
     private StopWatch watch;
 
+    /**
+     * Sets this watch to the inputted time.
+     * @param watch Type StopWatch used to display time.
+     */
     public FiveMinCommand(StopWatch watch) {
         this.watch = watch;
     }
-
+    /**
+     * Calls the watch to count down from 5 minutes to 0.
+     */
+    @Override
     public void execute(){
         watch.countDownFiveMin();
     }
