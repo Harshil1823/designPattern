544b501dde98d0eb7ea51b6c84c755305c0809c9
Dominick Ruggiero <domrugg124@gmail.com>
2023-02-24 15:20:35 -0500

    Fixed error, time was stopping at 00:01

--- command/StopWatch.java
+++ command/StopWatch.java
@@ -1,6 +1,6 @@
 package command;
-import java.util.concurrent.TimeUnit;
 
+import java.util.concurrent.TimeUnit;
 
 public class StopWatch {
     private int minutes;
@@ -10,21 +10,25 @@ public class StopWatch {
         minutes = 0;
         seconds = 0;
     }
+
     public void countDownOneMin() {
         minutes = 1;
         seconds = 0;
         countDown();
     }
+
     public void countDownFiveMin() {
         minutes = 5;
         seconds = 0;
         countDown();
     }
+
     public void countDownThirtySec() {
         minutes = 0;
         seconds = 30;
         countDown();
     }
+
     public void countDown() {
         try {
             while (minutes > 0 || seconds > 0) {
@@ -35,12 +39,19 @@ public class StopWatch {
                 TimeUnit.MILLISECONDS.sleep(100);
 
                 if (seconds == 0) {
+                    if (minutes == 0) {
+                        break;
+                    }
                     seconds = 59;
                     minutes--;
                 } else {
                     seconds--;
                 }
             }
+            // Printing final time (00:00)
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            System.out.printf("%02d:%02d%n", minutes, seconds);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
