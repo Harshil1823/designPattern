efd804a878b461dfe78616b49ae4c12c2c050c9a
manan3600 <manan@email.sc.edu>
2023-02-26 15:15:22 -0500

    java docing

--- command/StopWatch.java
+++ command/StopWatch.java
@@ -2,33 +2,54 @@ package command;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * The StopWatch class is responsible for counting down the time and displaying it on the console.
+ * It also contains methods for setting the countdown time to one minute, five minutes, or thirty seconds.
+ * @author javadoc
+ */
 public class StopWatch {
     private int minutes;
     private int seconds;
 
+    /**
+     * The StopWatch constructor initializes the minutes and seconds variables to zero.
+     */
     public StopWatch() {
         minutes = 0;
         seconds = 0;
     }
 
+    /**
+     * The countDownOneMin method sets the countdown time to one minute and calls the countDown method.
+     */
     public void countDownOneMin() {
         minutes = 1;
         seconds = 0;
         countDown();
     }
 
+    /**
+     * The countDownFiveMin method sets the countdown time to five minutes and calls the countDown method.
+     */
     public void countDownFiveMin() {
         minutes = 5;
         seconds = 0;
         countDown();
     }
 
+    /**
+     * The countDownThirtySec method sets the countdown time to thirty seconds and calls the countDown method.
+     */
     public void countDownThirtySec() {
         minutes = 0;
         seconds = 30;
         countDown();
     }
 
+    /**
+     * The countDown method decrements the countdown time and displays it on the console until it reaches zero.
+     * It uses a try-catch block to handle interruptions from sleeping threads.
+     */
     public void countDown() {
         try {
             while (minutes > 0 || seconds > 0) {
@@ -57,3 +78,4 @@ public class StopWatch {
         }
     }
 }
+
