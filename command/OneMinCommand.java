a9ccf205a7ebd0aa29733b4eb456146a2bde85eb
manan3600 <manan@email.sc.edu>
2023-02-26 15:05:20 -0500

    Java Docing

--- command/OneMinCommand.java
+++ command/OneMinCommand.java
@@ -1,13 +1,29 @@
 package command;
 
-public class OneMinCommand implements Command{
+/**
+ * The OneMinCommand class is responsible for decrementing the StopWatch by one minute when executed.
+ */
+public class OneMinCommand implements Command {
+
+    /**
+     * The watch instance variable is the StopWatch that the command will operate on.
+     */
     private StopWatch watch;
 
+    /**
+     * The OneMinCommand constructor initializes the watch instance variable.
+     *
+     * @param watch The StopWatch object that the command will operate on.
+     */
     public OneMinCommand(StopWatch watch) {
         this.watch = watch;
     }
+
+    /**
+     * The execute method decrements the StopWatch by one minute.
+     */
     public void execute() {
        watch.countDownOneMin();
     }
-    
 }
+
