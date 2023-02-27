08a462725f0857c63a5a6f0e01fec732831781db
manan3600 <manan@email.sc.edu>
2023-02-26 19:42:46 -0500

    Adding output.png and javadocing

--- command/ThirtySecCommand.java
+++ command/ThirtySecCommand.java
@@ -1,14 +1,29 @@
 package command;
 
-public class ThirtySecCommand implements Command{
+/**
+ * The ThirtySecCommand class is responsible for setting the StopWatch countdown time to thirty seconds and executing it.
+ */
+public class ThirtySecCommand implements Command {
+
+    /**
+     * The watch instance variable is the StopWatch that the command will operate on.
+     */
     private StopWatch watch;
 
+    /**
+     * The ThirtySecCommand constructor initializes the watch instance variable.
+     *
+     * @param watch The StopWatch object that the command will operate on.
+     */
     public ThirtySecCommand(StopWatch watch) {
         this.watch = watch;
     }
 
+    /**
+     * The execute method sets the StopWatch countdown time to thirty seconds and executes it.
+     */
     public void execute() {
         watch.countDownThirtySec();
     }
-    
 }
+
